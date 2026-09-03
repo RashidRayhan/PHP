a2f75375913d681addc08fb859931d7436d7b747
Rashid Rayhan <58268912+RashidRayhan@users.noreply.github.com>
2026-09-03 17:07:57 +0600

    Initial commit

---
+++ Variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    $x = "jhon"; //In PHP, a variable starts with the $ sign, followed by the name of the variable:
+    $y = 5;
+    $txt = "W3schools.com";
+    $a = 5;
+    $b = 6;
+
+    echo "My name is $x <br>";
+    echo "My age is $y years old <br>";
+    echo "I love $txt <br>";
+    echo 'the result is ' .$a + $b.;
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <title>This is PHP Header</title>
+    </header>
+    <body>
+        <?php
+            echo 'This is php script';
+        ?>
+    </body>
+</html>
