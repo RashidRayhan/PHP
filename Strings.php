a58db79cf56e705beecbdc3b237fcfd78e672cbf
Rashid Rayhan <58268912+RashidRayhan@users.noreply.github.com>
2026-09-05 17:00:35 +0600

    Create Strings.php

---
+++ Strings.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <title>The php string setting</title>
+    </header>
+<body>
+    <?php
+    echo "Hello world";
+
+    $x = "jhon";
+    echo "Hello $x";
+
+    $y = "Rashid";
+    echo "Hello $y";
+
+    $z = "Rayhan";
+    echo "Hello $z \n";
+    $a = "Rahim";
+    echo "Hello $a \r";
+    echo "world $a \t";
+    echo " Print $a \$";
+    ?>
+</body>
+</html>
